ecdf9778a578f4aa7d96f25a7a9b4c72d969f0ce
Giancotsu <gianfrancolarocca2797@gmail.com>
2023-03-09 14:33:32 +0100

    Adding "Libretto" and "Voto" models

---
+++ src/main/java/com/example/librettovoti/model/Libretto.java
@@ -0,0 +1,23 @@
+package com.example.librettovoti.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Libretto {
+
+    private List<Voto> voti;
+
+    private Libretto(){
+        this.voti = new ArrayList<>();
+    }
+
+    /**
+     *  Aggiungi un nuovo voto al libretto
+     *  (per ora non fa nessun controllo)
+     * @param voto è il voto da aggiungere
+     * @return true
+     */
+    public boolean add(Voto voto){
+        return this.voti.add(voto);
+    }
+}
---
+++ src/main/java/com/example/librettovoti/model/Voto.java
@@ -0,0 +1,43 @@
+package com.example.librettovoti.model;
+
+import java.time.LocalDate;
+
+public class Voto {
+
+    private String nomeCorso;
+    private Integer voto;
+    private LocalDate dataEsame;
+
+    public Voto() {
+    }
+
+    public Voto(String nomeCorso, Integer voto, LocalDate dataEsame) {
+        this.nomeCorso = nomeCorso;
+        this.voto = voto;
+        this.dataEsame = dataEsame;
+    }
+
+    public String getNomeCorso() {
+        return nomeCorso;
+    }
+
+    public void setNomeCorso(String nomeCorso) {
+        this.nomeCorso = nomeCorso;
+    }
+
+    public Integer getVoto() {
+        return voto;
+    }
+
+    public void setVoto(Integer voto) {
+        this.voto = voto;
+    }
+
+    public LocalDate getDataEsame() {
+        return dataEsame;
+    }
+
+    public void setDataEsame(LocalDate dataEsame) {
+        this.dataEsame = dataEsame;
+    }
+}
