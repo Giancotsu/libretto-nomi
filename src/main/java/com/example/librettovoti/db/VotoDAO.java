a0c2832cd561d0956c24ae7160b8b35619e0254f
Giancotsu <gianfrancolarocca2797@gmail.com>
2023-04-06 23:49:03 +0200

    Init VotoDAO

--- src/main/java/com/example/librettovoti/db/VotoDAO.java
+++ src/main/java/com/example/librettovoti/db/VotoDAO.java
@@ -1,4 +1,20 @@
 package com.example.librettovoti.db;
 
+import com.example.librettovoti.model.Voto;
+
+import java.util.List;
+
 public class VotoDAO {
+
+    public List<Voto> getVoti(){
+
+    }
+
+    public Voto getVotoByNomeCorso(String nomeCorso){
+
+    }
+
+    public boolean insVoto(Voto voto){
+
+    }
 }
