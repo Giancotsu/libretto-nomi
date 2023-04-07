2125f8b3a7a464bb96917603fb8484921f57b0da
Giancotsu <gianfrancolarocca2797@gmail.com>
2023-04-07 19:37:05 +0200

    Completed getVoti (from database) method

--- src/main/java/com/example/librettovoti/db/VotoDAO.java
+++ src/main/java/com/example/librettovoti/db/VotoDAO.java
@@ -3,7 +3,10 @@ package com.example.librettovoti.db;
 import com.example.librettovoti.model.Voto;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VotoDAO {
@@ -12,10 +15,23 @@ public class VotoDAO {
 
         try {
             Connection connection = DBConnect.getConnection();
+            String query = "SELECT * FROM voto";
+
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet resultSet = ps.executeQuery();
+
+            ArrayList<Voto> voti =  new ArrayList<Voto>();
+
+            while(resultSet.next()){
+                Voto votoDB = new Voto(resultSet.getString("nome_corso"), resultSet.getInt("voto"), null);
+                voti.add(votoDB);
+            }
+
+            return voti;
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return null;
     }
 
     public Voto getVotoByNomeCorso(String nomeCorso){
